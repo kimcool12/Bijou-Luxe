7a499f099a0f195b35a9b1ca63ffcbbb7cd67b09
kimcool12 <olatundeafolayan419@gmail.com>
2025-12-27 22:46:36 +0100

    feat: Enable Vercel deployment by adding rewrite configuration and exporting the Express app.

--- server.js
+++ server.js
@@ -148,6 +148,10 @@ app.post('/api/products', (req, res) => {
     res.json(newProduct);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
